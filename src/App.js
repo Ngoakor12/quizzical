80a3c067c8530143e1648b3cc9a76f5a1e9c3e83
ngoakor12 <ngoakor12@gmail.com>
2021-11-26 13:48:57 +0200

    deployment test done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import logo from "./assets/logo512.png";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+      <h1>Hello React!</h1>
+    </div>
+  );
 }
 
 export default App;
