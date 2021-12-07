4979df55d7b2ceb5f436d8d4d831896c75e8a12a
ngoakor12 <ngoakor12@gmail.com>
2021-12-07 09:52:24 +0200

    update title description

--- src/components/StartScreen/StartScreen.js
+++ src/components/StartScreen/StartScreen.js
@@ -3,12 +3,18 @@ import Form from "../Form/Form";
 const StartScreen = (props) => {
   return (
     <div className="start-screen">
-      <h1 className="quiz-title">Quizzical</h1>
-      <p className="quiz-description">A simple general knowledge trivia game</p>
-      <Form handleFormChange={props.handleFormChange} form={props.form} />
-      <button className="start-quiz-button" onClick={props.startGame}>
-        Start Game
-      </button>
+      <header className="startscreen-header">
+        <h1 className="quiz-title">Quizzical</h1>
+        <p className="quiz-description">
+          A trivia game for learning and testing yourself on different topics
+        </p>
+      </header>
+      <div className="game-controls">
+        <Form handleFormChange={props.handleFormChange} form={props.form} />
+        <button className="start-quiz-button" onClick={props.startGame}>
+          Start Game
+        </button>
+      </div>
       <Footer />
     </div>
   );
